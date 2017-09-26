80d13c37dbd79fe53434c5767c17a801c82a8d26
Felipedlr97 <31553358+Felipedlr97@users.noreply.github.com>
2017-09-26 14:07:35 -0500

    DaliaBusson
    Ejemplo de sistema con manejo de interfaces pero necesita de la conexion con el servidor, en este caso el ejemplo funciona con Xammp utilizando un servidor Apache.

---
+++ SYSTEMSTORE/src/systemstore/SYSTEM.java
@@ -0,0 +1,9 @@
+
+package systemstore;
+
+public interface SYSTEM {
+    public double costo=0.0;
+    public double precio[]=new double [100];
+    public String name="";
+    public String producto[]=new String[100];
+}
